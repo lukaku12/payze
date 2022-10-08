eae65c91230f1f609764285853b498ea75f37b3b
Your Name <lukakurdadze2@gmail.com>
2022-10-08 22:37:50 +0400

    style: add become loaner modal

--- resources/js/Components/BecomeLander.vue
+++ resources/js/Components/BecomeLander.vue
@@ -4,16 +4,32 @@
         გახდი გამსესხებელი
     </button>
 
-    <div @click="closeModal" v-if="showModal" class="absolute w-screen h-screen bg-[rgba(0,0,0,0.6)] top-0 left-0 !ml-0 z-40">
+    <div @click="closeModal" v-if="showModal"
+         class="absolute w-screen h-screen bg-[rgba(0,0,0,0.6)] top-0 left-0 !ml-0 z-40">
     </div>
     <div
         v-if="showModal"
-        class="absolute top-1/2 left-1/2 w-[50%] h-[70%] -translate-x-1/2 z-50 -translate-y-1/2 flex flex-col items-center border border-2 bg-emerald-500 rounded-xl">
-        <div class="mt-4  w-3/4">
-            <InputLabel for="TODO" value="TODO"/>
-            <TextInput id="TODO" type="text" class="mt-1 block w-full" v-model="card.name" required
-                       autocomplete="current-password"/>
+        class="absolute max-w-[700px] top-1/2 left-1/2 w-[80%] py-16 -translate-x-1/2 z-50 -translate-y-1/2 flex flex-col items-center border border-2 border-purple-600 bg-white rounded-xl">
+        <div class="mt-4 w-3/4">
+            <InputLabel for="card_name" value="Card Name"/>
+            <TextInput id="card_name" type="text" class="mt-1 block w-full border border-black" v-model="card.name" required placeholder="Soul Goodman"
+                       autocomplete="card_name"/>
             <InputError class="mt-2" :message="card.name"/>
+        </div>
+        <div class="mt-4 w-3/4">
+            <select class="w-full rounded">
+                <option selected disabled>აირჩიე ვალუტა</option>
+                <option value="1">USD</option>
+                <option value="2">GEL</option>
+            </select>
+        </div>
+        <div class="mt-5 w-3/4 relative h-[40px]">
+            <button
+                class="bg-purple-700 absolute right-0 flex justify-between items-center px-4 py-2.5 mt-[10px] rounded-xl font-bold hover: text-white cursor-pointer">
+                <a href="#">
+                    ბარათის დამატება
+                </a>
+            </button>
         </div>
     </div>
 </template>
@@ -23,9 +39,10 @@
 import InputLabel from "@/Components/InputLabel.vue";
 import TextInput from "@/Components/TextInput.vue";
 import InputError from "@/Components/InputError.vue";
+import Checkbox from "@/Components/Checkbox.vue";
 
 export default {
-    components: {TextInput, InputLabel, InputError},
+    components: {Checkbox, TextInput, InputLabel, InputError,},
 
     data() {
         return {
