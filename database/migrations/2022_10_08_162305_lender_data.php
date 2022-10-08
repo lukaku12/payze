7c7f4e99d4f67dee17f9222bccca3a72e29d6ef2
Your Name <lukakurdadze2@gmail.com>
2022-10-08 21:30:49 +0400

    feat: add migrations && wip: add modal to become loaner

--- database/migrations/2022_10_08_162305_lender_data.php
+++ database/migrations/2022_10_08_162305_lender_data.php
@@ -13,15 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('lender_data', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_lender');
-            $table->integer('reputation');
-            $table->rememberToken();
+            $table->foreignId('user_id')->constrained();
+            $table->bigInteger('max_amount_to_lend');
+            $table->bigInteger('left_limit');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -34,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 };
